080b039c77fe01fd8d99b6f015569f446088be7f
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2014-02-23 13:20:11 -0200

    better organization for middlewares, and redirect from herokuapp to our custom domain

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    middleware = {
+        manifestMiddleware: function (request, response, next) {
+            if (request.path === '/manifest.appcache') {
+                response.set('Content-Type', 'text/cache-manifest');
+            }
+            next.apply(this);
+        },
+        redirectToDomain: function (request, response, next) {
+            if (request.host === 'cargas.herokuapp.com') {
+                response.redirect(301, 'http://www.cargasapp.com');
+                response.end();
+            } else {
+                next.apply(this);
+            }
+        },
+        sendIndex: function (request, response, next) {
+            response.sendfile('index.html');
+        }
+    };
 
 app.configure(function () {
+    app.use(middleware.redirectToDomain);
+
     app.use(express.static(__dirname));
 
-    app.use(function (request, response, next) {
-        if (request.path === '/manifest.appcache') {
-            response.set('Content-Type', 'text/cache-manifest');
-        }
-        next.apply(this);
-    });
+    app.use(middleware.manifestMiddleware);
 
-    app.use(function (request, response, next) {
-        response.sendfile('index.html');
-    });
+    app.use(middleware.sendIndex);
 });
 
 app.listen(process.env.PORT || 3001);
