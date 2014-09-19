e4d44eee3263aad9ff811308efcc5361934fec8f
Gustavo Canete <gcanete@netsuite.com>
2014-09-19 11:27:50 -0300

    modal for fuel in refuels, watch fix in refuels, object or id?

--- www/js/modules/refuels/controllers/Refuels.Form.js
+++ www/js/modules/refuels/controllers/Refuels.Form.js
@@ -4,11 +4,12 @@ angular.module('refuels')
     '$scope',
     '$ionicPopup',
     '$state',
+    '$ionicModal',
     'Refuel',
     'Car',
     'Fuel',
     'utils',
-    function ($scope, $ionicPopup, $state, Refuel, Car, Fuel, utils) {
+    function ($scope, $ionicPopup, $state, $ionicModal, Refuel, Car, Fuel, utils) {
 
         var refuelDate;
 
@@ -18,7 +19,8 @@ angular.module('refuels')
 
         refuelDate = $scope.refuel.date ? new Date($scope.refuel.date) : new Date();
 
-        $scope.fuels = Fuel.query();
+        // $scope.fuels = Fuel.query();
+        $scope.fuels = _.sortBy(Fuel.query(), 'name');
         $scope.cars = Car.query();
 
         $scope.refuel.date = utils.formatDateForInput(refuelDate);
@@ -30,12 +32,16 @@ angular.module('refuels')
             })
         );
 
-        $scope.fuels = _.object(
+        $scope.refuel.fuel = $scope.refuel.fuel ? _.findWhere($scope.fuels, {
+            _id: $scope.refuel.fuel._id
+        }) : '';
+
+        /*$scope.fuels = _.object(
             _.pluck($scope.fuels, '_id'),
             _.map($scope.fuels, function (fuel) {
                 return fuel.name;
             })
-        );
+        );*/
 
         $scope.addNewCar = function () {
             $state.go('app.carNew');
@@ -53,22 +59,63 @@ angular.module('refuels')
             });
         };
 
-        $scope.$watch('refuel.fuel', function (fuel) {
+        /*$scope.$watch('refuel.fuel', function (fuel) {
             $scope.refuel.fuelObject = Fuel.get({
                 _id: parseInt(fuel)
             });
-        });
+        });*/
 
-        $scope.$watch('refuel.fuelObject.price * refuel.capacity', function (amount) {
+        $scope.$watch('refuel.fuel.price * refuel.capacity', function (amount) {
             $scope.refuel.amount = !isNaN(amount) ? Math.round(amount * 100) / 100 : '';
         });
 
-        $scope.$watch('refuel.car', function (carId) {
-            if (carId) {
-                $scope.refuel.fuel = Car.get({
-                    _id: parseInt(carId)
-                }).fuel;
+        // TODO: this is not correct when enter to the page in edit mode, the refuel.fuel is overlapped by car.fuel
+        $scope.$watch('refuel.car', function (newValue, oldValue) {
+            //if (carId) {
+            // console.log($scope.refuel.car);
+            if (newValue !== oldValue) {
+                var car = Car.get({
+                    _id: parseInt(newValue, 10)
+                });
+                console.log('new value: ', newValue);
+                console.log('old value: ', oldValue);
+                $scope.refuel.fuel = car && car.fuel ? _.findWhere($scope.fuels, {
+                    _id: car.fuel._id
+                }) : '';
             }
+            //}
+        });
+
+        // create fuel modal
+        $ionicModal.fromTemplateUrl('templates/fuels/new-modal.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.fuelModal = modal;
+        });
+
+        $scope.addNewFuel = function () {
+            // $state.go('app.fuelNew');
+            $scope.fuel = new Fuel();
+            $scope.fuelModal.show();
+        };
+
+        $scope.createFuel = function () {
+            $scope.fuel.$save(function () {
+                $scope.fuels = _.sortBy(Fuel.query(), 'name');
+                /*$scope.fuels = _.object(
+                    _.pluck($scope.fuels, '_id'),
+                    _.pluck($scope.fuels, 'name')
+                );*/
+                $scope.refuel.fuel = _.findWhere($scope.fuels, {
+                    _id: $scope.fuel._id
+                });
+                $scope.fuelModal.hide();
+            });
+        };
+
+        // remove modal instances from DOM
+        $scope.$on('$destroy', function () {
+            $scope.fuelModal.remove();
         });
 
     }
