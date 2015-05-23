c8c7fe6a251d0a333a9b66ef3dc7dc02d8de773e
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2015-05-23 17:31:24 -0300

    improved jshintrc file

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,55 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    bower = require('bower'),
-    concat = require('gulp-concat'),
-    sass = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    sh = require('shelljs'),
-    jshint = require('gulp-jshint'),
-    prettify = require('gulp-jsbeautifier'),
-    paths = {
-        sass: ['./scss/**/*.scss'],
-        scripts: './www/js/**/*.js'
-    };
+(function () {
+    'use strict';
 
-gulp.task('default', ['sass', 'hint']);
+    var gulp = require('gulp'),
+        gutil = require('gulp-util'),
+        bower = require('bower'),
+        sass = require('gulp-sass'),
+        minifyCss = require('gulp-minify-css'),
+        rename = require('gulp-rename'),
+        sh = require('shelljs'),
+        paths = {
+            sass: ['./scss/**/*.scss'],
+            scripts: './www/js/**/*.js'
+        };
 
-gulp.task('sass', function (done) {
-    gulp.src('./scss/ionic.app.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./www/css/'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./www/css/'))
-        .on('end', done);
-});
+    gulp.task('default', ['sass', 'hint']);
 
-gulp.task('watch', function () {
-    gulp.watch(paths.sass, ['sass']);
-});
+    gulp.task('sass', function (done) {
+        gulp.src('./scss/ionic.app.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('./www/css/'))
+            .pipe(minifyCss({
+                keepSpecialComments: 0
+            }))
+            .pipe(rename({
+                extname: '.min.css'
+            }))
+            .pipe(gulp.dest('./www/css/'))
+            .on('end', done);
+    });
 
-gulp.task('install', ['git-check'], function () {
-    return bower.commands.install()
-        .on('log', function (data) {
-            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-        });
-});
+    gulp.task('watch', function () {
+        gulp.watch(paths.sass, ['sass']);
+    });
 
-gulp.task('git-check', function (done) {
-    if (!sh.which('git')) {
-        console.log(
-            '  ' + gutil.colors.red('Git is not installed.'),
-            '\n  Git, the version control system, is required to download Ionic.',
-            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-        );
-        process.exit(1);
-    }
-    done();
-});
+    gulp.task('install', ['git-check'], function () {
+        return bower.commands.install()
+            .on('log', function (data) {
+                gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+            });
+    });
 
-gulp.task('beautify', function () {
-    return gulp.src(paths.scripts)
-        .pipe(prettify({
-            config: '.jsbeautifyrc',
-            mode: 'VERIFY_AND_WRITE'
-        }))
-        .pipe(gulp.dest('./www/js'));
-});
-
-gulp.task('hint', ['beautify'], function () {
-    return gulp.src(paths.scripts)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'));
-});
+    gulp.task('git-check', function (done) {
+        if (!sh.which('git')) {
+            console.log(
+                '  ' + gutil.colors.red('Git is not installed.'),
+                '\n  Git, the version control system, is required to download Ionic.',
+                '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+                '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+            );
+            process.exit(1);
+        }
+        done();
+    });
+})();
